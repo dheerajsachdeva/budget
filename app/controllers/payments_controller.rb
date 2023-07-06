54fdbad0ad8de802176bdc7a014d7f1fa603fc4c
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-07-06 22:25:05 +0530

    Adding files for controllers

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+  end
+
+ 
+      def new
+        @payment = Payment.new
+      end
+
+
+  def create
+    @payment = Payment.new(payment_params)
+    @payment.author_id = current_user.id
+    if @payment.save
+        flash[:success] = 'Payment succesfully added'
+        redirect_to category_path(payment_params[:category_id])
+      else
+        flash[:error] = 'Error: payment could not be added'
+        render :new
+      end
+  end
+
+  
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount, :category_id)
+  end
+
+
+  end
